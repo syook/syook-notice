b6f2ee1a0aa6c2ae689163545a96dd279f910c1e
anik3tra0 <anik3tra0@gmail.com>
2017-12-27 10:47:06 +0530

    Enhancements for new line characters

--- lib/syook_notice.rb
+++ lib/syook_notice.rb
@@ -1,21 +1,20 @@
 	require 'json'
 	require 'httparty'
-	require 'pry'
 	class SyookNotice
-		attr_accessor :list ,:msg
 		@@header = { "Content-Type" => "application/json", "Authorization" => "Bearer #{ENV["SENDGRID_SYOOK"]}" }
 
-		def initialize(list, msg)
-			@list = list
+		def initialize(list, msg, subject)
+			@list = list.map(&:to_json)
 			@msg = msg
+			@subject = subject
 		end
 
 		def sendmsg
-			binding.pry
 			@list.each do |obj|
 				obj = JSON.parse(obj)
 				regex = Regexp.union(obj.keys)
-				msg.gsub!(regex,obj)
+				msg.gsub!(regex, obj)
+				msg.gsub!("\n", "<br>")
 				json = {
 								"personalizations"=> [
 									{
@@ -26,7 +25,7 @@
 										]
 									}
 								],
-								"subject" => obj["subject"],
+								"subject" => @subject,
 								"content" => [
 															{
 																"type" => "text/html",
@@ -51,3 +50,4 @@ end
 	# s = SyookNotice.new((l.map{|n| n.to_json}), msg )
 	# s.sendmsg
 # msg = "Dear name ji,\n\nWe take this opportunity to thank you for your business. 2017 has been a great year for us and hope it has been the same for you too.\n\nWith a vision to constantly improve your Syook experience, there will be a minor change in the link to access your account. If your current website is subdomain.syook.com, it will now change to subdomain.syookpro.com. There will also be an update to the mobile app. For this week, both the links will work. Starting Jan 2nd, the link of subdomain.syook.com will stop working.\n\nEverything else remains the same. There is no change in the way you use the system.\n\nThe entire Syook team would also like to take this opportunity to wish you a very happy and prosperous new year 2018."
+
