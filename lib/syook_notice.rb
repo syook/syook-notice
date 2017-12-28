9f7c3c40b160e5b505bc1c7492be43176a7ef025
Pavan Kumar <kumarpavan1309@gmail.com>
2017-12-28 11:27:30 +0530

    Bug Fixed

--- lib/syook_notice.rb
+++ lib/syook_notice.rb
@@ -10,12 +10,14 @@
 		end
 
 		def sendmsg
-			@list.each do |obj|
+			@list.each do |l|
+				obj = l
 				obj = JSON.parse(obj)
 				obj["name"] = obj["name"].split(" ").map(&:capitalize).join(" ")
 				regex = Regexp.union(obj.keys)
-				@msg.gsub!(regex, obj)
-				@msg.gsub!("\n", "<br>")
+				msg = @msg
+				msg = msg.gsub(regex, obj)
+				msg = msg.gsub("\n", "<br>")
 				json = {
 								"personalizations"=> [
 									{
@@ -30,7 +32,7 @@
 								"content" => [
 															{
 																"type" => "text/html",
-																"value" => "#{@msg}"
+																"value" => "#{msg}"
 															}
 														],
 								"from"=> {
@@ -38,17 +40,24 @@
 								},
 								"template_id" => "c1e54d1c-72da-451c-9ee4-afa2938f695d"
 								}
-			response = HTTParty.post("https://api.sendgrid.com/v3/mail/send", headers: @@header, body: json.to_json )
-			puts "done"
-			puts response
+				if (msg.include?( obj["name"]) && msg.include?(obj["subdomain"]))
+					puts "sending to #{obj["name"]} of #{obj["subdomain"]}"
+					response = HTTParty.post("https://api.sendgrid.com/v3/mail/send", headers: @@header, body: json.to_json )
+					puts response
+					sleep 3
+					next
+				end
+				puts "Going to next"
+				next
+			
 		end
 	end
 end
 
-	data =  [ { name: 'aniket rao', email: 'aniket@syook.com', subdomain: 'sandbox' }, { name: 'tejas gowda', email: 'tejas@syook.com', subdomain: 'sandbox'},{	name: 'aman agarwal',email: 'aman@syook.com',subdomain: 'sandbox'},{name: 'arjun nagarajan' , email: 'arjun@syook.com',subdomain: 'sandbox' },{name: 'pallavi hegde' , email: 'pallavi@syook.com',subdomain: 'sandbox' },{name: 'saurabh' , email: 'saurabh@syook.com',subdomain: 'sandbox' },{name: 'nithil' , email: 'nithil@syook.com',subdomain: 'sandbox' },{name: 'rachit aggarwal' , email: 'rachit@syook.com',subdomain: 'sandbox' },{name: 'meghal' , email: 'meghal@syook.com',subdomain: 'sandbox' }]
+	# data =  [ { name: 'aniket rao', email: 'aniket@syook.com', subdomain: 'sandbox' }, { name: 'tejas gowda', email: 'tejas@syook.com', subdomain: 'randbox'},{	name: 'aman agarwal',email: 'aman@syook.com',subdomain: 'newbox'},{name: 'arjun nagarajan' , email: 'arjun@syook.com',subdomain: 'gearbox' },{name: 'pallavi hegde' , email: 'pallavi@syook.com',subdomain: 'nobox' },{name: 'saurabh' , email: 'saurabh@syook.com',subdomain: 'wrongbox' },{name: 'nithil' , email: 'nithil@syook.com',subdomain: 'somebox' },{name: 'rachit aggarwal' , email: 'rachit@syook.com',subdomain: 'lunchbox' },{name: 'meghal' , email: 'meghal@syook.com',subdomain: 'sabox' }]
 
-	msg = "Dear name ji,\n\nWe take this opportunity to thank you for your business. 2017 has been a great year for us and hope it has been the same for you too.\n\nWith a vision to constantly improve your Syook experience, there will be a minor change in the link to access your account. If your current website is subdomain.syook.com, it will now change to subdomain.syookpro.com. There will also be an update to the mobile app. For this week, both the links will work. Starting Jan 2nd, the link of subdomain.syook.com will stop working.\n\nEverything else remains the same. There is no change in the way you use the system.\n\nThe entire Syook team would also like to take this opportunity to wish you a very happy and prosperous new year 2018."
+	# msg = "Dear name ji,\n\nWe take this opportunity to thank you for your business. 2017 has been a great year for us and hope it has been the same for you too.\n\nWith a vision to constantly improve your Syook experience, there will be a minor change in the link to access your account. If your current website is subdomain.syook.com, it will now change to subdomain.syookpro.com. There will also be an update to the mobile app. For this week, both the links will work. Starting Jan 2nd, the link of subdomain.syook.com will stop working.\n\nEverything else remains the same. There is no change in the way you use the system.\n\nThe entire Syook team would also like to take this opportunity to wish you a very happy and prosperous new year 2018."
 	
-	subject = "Changes to your subdomain"
-	s = SyookNotice.new(data, msg,subject )
-	s.sendmsg
+	# subject = "Changes to your subdomain"
+	# s = SyookNotice.new(data, msg,subject )
+	# s.sendmsg
